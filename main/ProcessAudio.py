2c96ed93599a20a51b35fc303f5dc05f87ee80fa
Anthony Erlinger <aerlinger@gmail.com>
2015-11-07 00:38:11 -0500

    Add python script to call zPlane API

---
+++ main/ProcessAudio.py
@@ -0,0 +1,131 @@
+import requests
+import time
+import os
+import json
+import glob
+
+from csvkit.convert import *
+from StringIO import StringIO
+
+accessId = '01ff3792-3f80-4ed6-ac51-2add16e0db68'
+baseUrl = 'https://api.sonicAPI.com/'
+
+
+def requestParams(audioFilename):
+  return {
+    'access_id': accessId,
+    'format': 'json',
+    'input_file': audioFilename
+  }
+
+
+def createTimestampedFolder(audioFilename, timestamp):
+  pathname = "data/" + timestamp + "/" + audioFilename + "/"
+  os.makedirs(pathname)
+
+  return pathname
+
+
+def analyzeChords(audioFilename, destinationPath):
+  print "Analyzing chords: " + audioFilename
+
+  response = requests.post(baseUrl + 'analyze/chords',
+                           data=requestParams(audioFilename),
+                           files={'input_file': open(audioFilename, 'rb')})
+
+  if response.status_code == 200:
+    chordsBody = response.json()['chords_result']['chords']
+
+    chordStrings = StringIO(json.dumps(chordsBody))
+    rawCsv = json2csv(chordStrings)
+
+    with open(destinationPath + "/chords.csv", "w") as f:
+      f.write(rawCsv)
+
+    with open(destinationPath + "/chords.json", "w") as f:
+      f.write(json.dumps(response.json()['chords_result']))
+
+    return response.json()['chords_result']
+  else:
+    print "Analyze chords request failed: " + response.text
+
+
+def analyzeMelody(audioFilename, destinationPath, detailed=False):
+  print "Analyzing melody " + audioFilename + " " + str(detailed)
+
+  params = requestParams(audioFilename)
+  params["detailed_result"] = "true" if detailed else "false"
+
+  response = requests.post(baseUrl + 'analyze/melody',
+                           data=params,
+                           files={'input_file': open(audioFilename, 'rb')})
+
+  suffix = "_detailed" if detailed else ""
+
+  if response.status_code == 200:
+    melodyBody = response.json()['melody_result']['notes']
+
+    rawCsv = json2csv(StringIO(json.dumps(melodyBody)))
+
+    with open(destinationPath + "/melody" + suffix + ".csv", "w") as f:
+      f.write(rawCsv)
+
+    with open(destinationPath + "/melody" + suffix + ".json", "w") as f:
+      f.write(json.dumps(response.json()['melody_result']))
+
+    return response.json()['melody_result']
+  else:
+    print "Analyze melody request failed: " + response.text
+
+
+def analyzeBeat(audioFilename, destinationPath):
+  print "Analyzing beat: " + audioFilename
+
+  response = requests.post(baseUrl + 'analyze/tempo',
+                           data=requestParams(audioFilename),
+                           files={'input_file': open(audioFilename, 'rb')})
+
+  if response.status_code == 200:
+    responseBody = response.json()['auftakt_result']['click_marks']
+
+    clickMarks = StringIO(json.dumps(responseBody))
+    rawCsv = json2csv(clickMarks)
+
+    with open(destinationPath + "/beat.csv", "w") as f:
+      f.write(rawCsv)
+
+    with open(destinationPath + "/beat.json", "w") as f:
+      f.write(json.dumps(response.json()['auftakt_result']))
+
+    return response.json()['auftakt_result']
+
+
+
+
+def processFile(filename, destinationPath):
+  chordsJson = analyzeChords(filename, destinationPath)
+  melodyJson = analyzeMelody(filename, destinationPath)
+  detailedMelodyJson = analyzeMelody(filename, destinationPath, True)
+  beatJson = analyzeBeat(filename, destinationPath)
+
+  meta = dict()
+
+  meta["clicks_per_bar"] = beatJson["clicks_per_bar"]
+  meta["overall_tempo"] = beatJson["overall_tempo"]
+  meta["overall_tempo_straight"] = beatJson["overall_tempo_straight"]
+
+  meta["key"] = melodyJson["key"]
+  meta["key_index"] = melodyJson["key_index"]
+  meta["tuning_frequency"] = melodyJson["tuning_frequency"]
+
+  with open(destinationPath + "/meta.json", "w") as f:
+        f.write(json.dumps(meta, indent=2))
+
+
+timestamp = str(int(time.time()))
+
+print("Creating destination path " + timestamp)
+
+for filename in glob.glob("data/process/*.mp3"):
+  processFile(filename, createTimestampedFolder(filename, timestamp))
+
--- main/main.pde
+++ main/main.pde
@@ -2,17 +2,28 @@ import processing.sound.*;
 
 SoundFile file;
 Table melodyTable;
+Table richMelodyTable;
 Table beatTable;
+Table chordTable;
 
 int frames = 0;
 float fps = 30.0;
-float pixelsPerSecond = 30.0;
+float pixelsPerSecond = 320.0;
+
+//String filename = "a-drowning.mp3";
+//String filename = "end-of-line.mp3";
+//String filename = "the-four-of-us-are-dying.mp3";
+//String filename = "clutter.mp3";
+//String filename = "the-believers.mp3";
+String filename = "cider_time.mp3";
 
 void setup() {
-  melodyTable = loadTable("data/clutter/melody.csv", "header");
-  beatTable = loadTable("data/clutter/beat.csv", "header");
+  chordTable = loadTable("data/_" + filename + "/chords.csv", "header");
+  melodyTable = loadTable("data/_" + filename + "/melody.csv", "header");
+  richMelodyTable = loadTable("data/_" + filename + "/melody_detailed.csv", "header");
+  beatTable = loadTable("data/_" + filename + "/beat.csv", "header");
   
-  size(1600, 360);
+  size(1800, 360);
   background(64);
   
   for (TableRow row : melodyTable.rows()) {
@@ -21,7 +32,7 @@ void setup() {
   
   frameRate(fps); 
   
-  file = new SoundFile(this, "clutter/audio.mp3");
+  file = new SoundFile(this, filename);
   file.play();
 }
 
@@ -42,20 +53,57 @@ void draw() {
   
   for (TableRow row : beatTable.rows()) {
     float beatX = pixelsPerSecond * row.getFloat("time");
+    boolean downbeat = row.getString("downbeat").equals("true");
     
     fill(color(0, 255, 0), 255);
     stroke(color(0, 255, 0), 255);
-    ellipse(beatX, height - 50, 3, 3);
+    
+    float radius = 3.0;
+    if(downbeat)
+      radius = 8.0;
+      
+    ellipse(beatX, height - 50, radius, radius);
   }
   
   for (TableRow row : melodyTable.rows()) {
     float noteX = pixelsPerSecond * row.getFloat("onset_time");
     float noteY = 2 * row.getFloat("midi_pitch");
+    float duration = row.getFloat("duration");
     
     fill(color(255, 0, 0), 255);
     stroke(color(255, 0, 0), 255);
+    rect(noteX, noteY, duration * pixelsPerSecond, 3);
+    //println(noteX + ": " + noteY);
+  }
+  
+  for (TableRow row : richMelodyTable.rows()) {
+    float noteX = pixelsPerSecond * row.getFloat("onset_time");
+    float noteY = 2 * row.getFloat("midi_pitch") - 15;
+    
+    fill(color(255, 0, 255), 255);
+    stroke(color(255, 0, 0), 255);
     ellipse(noteX, noteY, 3, 3);
-    println(noteX + ": " + noteY);
-    //println(row.getString("onset_time") + ": " + row.getString("midi_pitch"));
+    //println(noteX + ": " + noteY);
+  }
+  
+  for (TableRow row : richMelodyTable.rows()) {
+    float noteX = pixelsPerSecond * row.getFloat("onset_time");
+    float noteY = 2 * row.getFloat("midi_pitch") - 15;
+    
+    fill(color(255, 0, 255), 255);
+    stroke(color(255, 0, 0), 255);
+    ellipse(noteX, noteY, 3, 3);
+    //println(noteX + ": " + noteY);
+  }
+  
+  for (TableRow row : chordTable.rows()) {
+    float chordX = pixelsPerSecond * row.getFloat("time");
+    float chordY = height/2 - 15;
+    
+    fill(color(255, 0, 255), 255);
+    stroke(color(255, 0, 0), 255);
+    line(chordX, chordY - 20, chordX, chordY + 20);
+    
+    text(row.getString("chord"), chordX, chordY); 
   }
 }
